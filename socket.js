7a71ea9d1e758c8500d0f5e4667351286b5ce9e1
Martin Smith <smithdev1237@gmail.com>
2019-03-08 10:01:26 +0000

    Updated UI and made it so pins are removed when status is ok

--- socket.js
+++ socket.js
@@ -10,17 +10,68 @@ socket.on('connect', (socket) => {
     $("#spnStatus").removeClass("red");
 });
 
+function PinData() {
+    this.id;
+    this.pin;
+}
+
+var pinData = [];
+
 socket.on('alerts', (alert) => {
     console.log(alert);
     var data = JSON.parse(alert);
-    $("#tblStatus").append("<tr><td>"+data.name+"</td><td>"+data.status+"</td><td>"+data.text+"</td></tr>");
+    var className = "green";
     //{"deviceId":2,"lat":52.175741,"long":-3.104488,"name":"Alarm 2","status":"Ok","text":"non-person object has been spotted."}
 
-    var pinPosition = [data.long, data.lat];
-    map.markers.add(new atlas.HtmlMarker({
-        htmlContent: "<div><div class='pin bounce'></div><div class='pulse'></div><span class='markerName'>"+data.name+"</span></div>",
-        position: pinPosition,
-        pixelOffset: [5, -18],
-        name: data.name
-    }));
+
+    switch (data.status) {
+
+        case "Error":
+
+        className = "red";
+            var pinPosition = [data.long, data.lat];
+            var pin = new atlas.HtmlMarker({
+                htmlContent: "<div><div class='pin bounce'></div><div class='pulse'></div><span class='markerName'>" + data.name + "</span></div>",
+                position: pinPosition,
+                pixelOffset: [5, -18],
+                name: data.name
+            })
+
+            var found = false;
+            for (var i = 0; i < pinData.length; i++) {
+
+                if (pinData[i].id == data.deviceId) {
+                    found = true;
+                    break;
+                }
+            }
+           
+
+            if (found == false) {
+
+                var newData = new PinData();
+                newData.id = data.deviceId;
+                newData.pin = pin;
+                pinData.add()
+
+                map.markers.add(pin);
+            }
+
+            break;
+
+        case "Ok":
+
+            var found = false;
+            for (var i = 0; i < pinData.length; i++) {
+
+                if (pinData[i].id == data.deviceId) {
+                    map.markers.remove(pinData[i].pin);
+                    pinData.splice(i);
+                    break;
+                }
+            }
+            break;
+    }
+
+    $("#tblStatus").append("<tr class='" + className + "'><td>" + data.name + "</td><td>" + data.status + "</td><td>" + data.text + "</td></tr>");
 });
